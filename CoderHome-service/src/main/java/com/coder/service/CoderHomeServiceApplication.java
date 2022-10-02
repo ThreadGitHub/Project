26c89231c62b3bbcfda1870e07ba47f32a0eab02
Thread丶 <ThreadEmail@163.com>
2022-10-02 16:39:54 +0800

    提交.gitgnore

---
+++ CoderHome-service/src/main/java/com/coder/service/CoderHomeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coder.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderHomeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoderHomeServiceApplication.class, args);
+    }
+
+}
