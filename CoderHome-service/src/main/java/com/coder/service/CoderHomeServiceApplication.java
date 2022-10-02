19e020964d807b96132396b995b6d9b0a2fb8883
Thread丶 <ThreadEmail@163.com>
2022-10-02 16:53:47 +0800

    :sparkles: 提交分层

--- CoderHome-service/src/main/java/com/coder/service/CoderHomeServiceApplication.java
+++ CoderHome-service/src/main/java/com/coder/service/CoderHomeServiceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CoderHomeServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CoderHomeServiceApplication.class, args);
     }
-
 }
