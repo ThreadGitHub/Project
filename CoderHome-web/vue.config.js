250b262bc55399502fdf21f59ae0d2a41210790d
Thread丶 <ThreadEmail@163.com>
2022-10-06 16:00:30 +0800

    :sparkles: 增加后台代理地址

--- CoderHome-web/vue.config.js
+++ CoderHome-web/vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 设置vue服务器代理后台服务地址
+  devServer: {
+    host: "localhost",
+    port: "8080",
+    proxy: {
+      '/api' : {
+        target: 'http://localhost:9999',
+        changeOrigin: true,
+        ws: false
+      }
+    }
+  }
 })
